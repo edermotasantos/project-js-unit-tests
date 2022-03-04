c4b6fe435595f9f61659df588e8703542c742a22
Eder Santos <3derms@gmail.com>
2022-03-03 21:35:29 -0300

    fix: corrige createMenu

--- src/restaurant.js
+++ src/restaurant.js
@@ -79,10 +79,6 @@
 // soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, 
 // você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = (obj) => {
-  fetchMenu: () =>  {
-    console.log(obj);
-  }
-};
+const createMenu = (obj) => {};
 
 module.exports = createMenu;
